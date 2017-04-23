b971b921c566a29d06bfa7ddcd178376632bb967
RoboticsCSE180 <kbijani@ucmerced.edu>
2017-04-23 16:25:07 -0700

    Changed a line in hello.cpp to test repository

--- hello.cpp
+++ hello.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv){
 	ros::init(argc, argv, "hello_ros");
 	ros::NodeHandle nh; 
 
-	ROS_INFO_STREAM("HELLO ROS");  
+	ROS_INFO_STREAM("HELLO ROS, and HELLO GITHUB");  
 
 	return 0;
 }
