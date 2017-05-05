c1588d181cdaf978195d6d5ac3c1fd0608502430
RoboticsCSE180 <kbijani@ucmerced.edu>
2017-05-04 22:58:26 -0700

    Updated testcostmap.cpp

--- testcostmap.cpp
+++ testcostmap.cpp
@@ -1,9 +1,16 @@
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
-void OccupancyGridMsgReceived(const nav_msgs::OccupancyGrid &o){
 
-	ROS_INFO_STREAM("Data length: " << (sizeof(o.data) / sizeof(o.data[0])) << "\n"
-					<< "origin: " << o.info.origin.position.x << "\n");  
+void OccupancyGridMsgReceived(const nav_msgs::OccupancyGrid &o){
+	
+	int length = (sizeof(o.data) / sizeof(o.data[0])); 
+
+	for(int i = 0; i < length; ++i){
+		ROS_INFO("data: %d\n", o.data[i]); 
+		ROS_INFO_STREAM("more info: " <<  o.info.width << "\n"
+				<< "even more info: " << o.info.origin.position.x 
+				<< "\neven even more info: " << o.info.origin.position.y); 
+	}
 	
 	return; 
 }
@@ -14,9 +21,9 @@ int main(int argc, char** argv){
 
 	ros::NodeHandle nh; 
 
-	ros::Subscriber sub = nh.subscribe("/move_base/global/costmap/costmap", 1000, &OccupancyGridMsgReceived); 
+	ros::Subscriber sub = nh.subscribe("/move_base/local_costmap/costmap", 1000, &OccupancyGridMsgReceived); 
 	
-	ros::spinOnce(); 
+	ros::spin(); 
 
 	return 0; 
 }
